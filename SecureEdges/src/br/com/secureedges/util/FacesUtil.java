c5466e6bdcfa6fbe0e7df96fd82197aafde8b39e
Tenorio <Tenorio@192.168.1.4>
2017-02-01 19:37:17 -0200

    Crud Comodo

--- SecureEdges/src/br/com/secureedges/util/FacesUtil.java
+++ SecureEdges/src/br/com/secureedges/util/FacesUtil.java
@@ -1,8 +1,13 @@
 package br.com.secureedges.util;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 
+
+import java.util.Map;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 public class FacesUtil {
 	public static void adicionarMSGInfo(String mensagem) {
 		FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO,mensagem,mensagem);
@@ -17,5 +22,19 @@ public class FacesUtil {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		facesContext.addMessage(null, facesMessage);
 	}
+	public static String  getParam (String nome) {
+		// cria um contexto pegando o atual
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		// pega o contexto externo
+		ExternalContext externalContext = facesContext.getExternalContext();
+		// cria um mapa de paramtros 
+		Map<String, String> paramtros =externalContext.getRequestParameterMap();
+		// insere o nome no valor e retorna o valor
+		String valor = paramtros.get(nome);
+		return valor;
+		
+	}
+	
+	
 
 }
