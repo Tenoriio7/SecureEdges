2fb0fc12e74ec9f11e1de4585f485101c9f57f39
Tenorio <Tenorio@Tenorio-PC>
2017-06-02 13:19:40 -0300

    Correcoes grafico

--- SecureEdges/src/br/com/secureedges/domain/Grafico.java
+++ SecureEdges/src/br/com/secureedges/domain/Grafico.java
@@ -1,17 +1,30 @@
 package br.com.secureedges.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.Test;
 
 import br.com.secureedges.core.dao.DispositivoDAO;
 import br.com.secureedges.core.dao.RelatorioDAO;
 
-
 public class Grafico extends EntidadeDominio {
-	 
-	public RelatorioDAO relatorioDAO =  new RelatorioDAO();
+
+	public RelatorioDAO relatorioDAO = new RelatorioDAO();
 	public List<EntidadeDominio> relatorios = relatorioDAO.listar();
 	private DispositivoDAO dispositivoDAO = new DispositivoDAO();
-	public List<EntidadeDominio> dispositivos = dispositivoDAO.listar();
+	public List<EntidadeDominio> dispositivosaux = dispositivoDAO.listar();
+	public List<EntidadeDominio> dispositivos= new ArrayList<>();
+
+	
+	public Grafico() {
+		for(EntidadeDominio dispositivo :dispositivosaux){
+			
+			if(dispositivo instanceof Dispositivo);
+			dispositivos.add((Dispositivo)dispositivo);
+		}
+			
+		}
 	
 
 }
